2c98c93b16c58e2fc3e0b4345b3ab65adb024b34
ipa1987 <idris.patel1987@outlook.com>
2025-01-02 14:08:39 +0000

    Replaced jQuery elements with normal Javascript

--- obj/Debug/net8.0/BlazingBlogs.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazingBlogs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingBlogs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3808e2c1eea98ced384fcacdaa635860d6917d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingBlogs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingBlogs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/assets/js/functions.js
+++ wwwroot/assets/js/functions.js
@@ -1,7 +1,18 @@
-jQuery( document ).ready(function() {
-
-    $(window).scroll(function(){
-    $('.topnav').toggleClass('scrollednav py-0', $(this).scrollTop() > 50);
-    });
-    
+document.addEventListener("DOMContentLoaded", _ => {
+   const topNav = document.getElementsByName("topNav")[0];
+   if (topNav) {
+       window.onscroll = () => {
+           if (window.scrollY > 50) {
+               topNav.classList.add('scrolledNav', 'py-0')
+           }
+           else {
+               topNav.classList.remove('scrolledNav', 'py-0')
+           }
+       }
+   }
 });
+
+function toggleMenu(e) {
+    e.target.classList.toggle('collapsed');
+    document.getElementById('top-navbar-menu-wrapper').classList.toggle('show');
+}
